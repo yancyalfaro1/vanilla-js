dfcadc8c05ead9ebeac731aa2d1541c987fba594
yancyalfaro1 <yalfaro@fwdcostarica.com>
2024-08-14 10:09:32 -0600

    cambios

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,7 +1,8 @@
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+    return this.valor++;
+
   },
 };
 
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,13 @@
+import { contador } from "./ejercicio1"
+
 class Contador {
   // Inserte el código aquí
+valor=0
+  siguiente() {
+   return this.valor++;
+  }
 }
-
+const instancia = new Contador()
 // Inserte el código aquí
 
 export { instancia }
