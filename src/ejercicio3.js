8c0a1103009f8669a9c793761997e6da18a54d5b
yancyalfaro1 <yalfaro@fwdcostarica.com>
2024-08-16 11:18:19 -0600

    cambios

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,34 @@
-// Inserte el código aquí
+function clonarArreglo() {
+    
+}
 
+function clonarObjeto() {
+    
+}
 
+function sumar() {
+    
+}
+
+function saludar(nombre) {
+    
+}
 
 // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
+
+//1. Realice el ejercicio en el archivo: ejercicio3.js
+//2. Ejecute las pruebas con 'npm test' para garantizar que el código este funcionando correctamente.
+
+//### Ejercicios:
+//1. Escribir una función llamada clonarArreglo.
+ //  * Dado un arreglo, retorna una arreglo nuevo con todos los elementos del arreglo original. 
+//2. Escribir una función llamada clonarObjeto.
+  // * Dado un objeto, retorna un objeto nuevo con todos los elementos del objeto original.
+//3. Escribir una función llamada sumar.
+ //  * Debe recibir N parametros (rest operator)
+   //* Retorna la suma de todos los parametros
+//4. Escribir una función llamada saludar.
+  // * Debe recibir un parametro llamado nombre
+//   * El valor por defecto para el parametro nombre es "Desconocido"
+  // * La función retorna el mensaje "Hola <nombre>"
